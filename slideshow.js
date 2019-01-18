b30c14746daba470ad96b629df6bd2a014d9900b
True Ghiassi <true@ghiassitrio.co.uk>
2019-01-18 13:37:00 +0000

    I think it's done.

--- slideshow.js
+++ slideshow.js
@@ -15,9 +15,6 @@ var curIndex = 0,
         'topslideshow/19wisteria.jpg'];
 
 
-//
-// Dynamically add each image frame into the dom;
-//
 function buildSlideShow(arr) {
     for (i = 0; i < arr.length; i++) {
         var img = document.createElement('img');
@@ -27,10 +24,7 @@ function buildSlideShow(arr) {
     }
 }
 
-//
-// Our slideshow function, we can call this and it flips the image instantly, once it is called it will roll
-// our images at given interval [imgDuration];
-//
+
 function slideShow() {
     function fadeIn(e) {
         e.className = "fadeIn";
@@ -38,11 +32,9 @@ function slideShow() {
     function fadeOut(e) {
         e.className = "";
     };
-    // first we start the existing image fading out;
 
     fadeOut(slides[curIndex]);
 
-    // then we start the next image fading in, making sure if we are at the end we restart!
     curIndex++;
     if (curIndex == slides.length) {
         curIndex = 0;
@@ -50,13 +42,9 @@ function slideShow() {
 
     fadeIn(slides[curIndex]);
 
-    // now we are done we recall this function with a timer, simple.
-
     setTimeout(slideShow, imgDuration);
 };
 
-
-// first build the slider, then start it rolling!
 
 buildSlideShow(imgArray);
 slideShow();
