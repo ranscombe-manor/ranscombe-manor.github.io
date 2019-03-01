5945d38ba25c216691552093e6d6ec5e2ad54c34
True Ghiassi <true@ghiassitrio.co.uk>
2019-03-01 12:53:57 +0000

    Removed more webp files. I think it's all back to jpg by now.

--- slideshow.js
+++ slideshow.js
@@ -3,16 +3,16 @@ var curIndex = 0,
     slider = document.getElementById("slider"),
     slides = slider.childNodes;
     imgArray = [
-        'topslideshow/20wisteria.webp',
-        'topslideshow/02parterre.webp',
-        'topslideshow/01arialview.webp',
-        'topslideshow/09pinkflower.webp',
-        'topslideshow/mainImage.webp',
-        'topslideshow/lounge2.webp',
-        'topslideshow/10ivy.webp',
-        'topslideshow/14ivy.webp',
-        'topslideshow/15fern.webp',
-        'topslideshow/19wisteria.webp'];
+        'topslideshow/20wisteria.jpg',
+        'topslideshow/02parterre.jpg',
+        'topslideshow/01arialview.jpg',
+        'topslideshow/09pinkflower.jpg',
+        'topslideshow/mainImage.jpg',
+        'topslideshow/lounge2.jpg',
+        'topslideshow/10ivy.jpg',
+        'topslideshow/14ivy.jpg',
+        'topslideshow/15fern.jpg',
+        'topslideshow/19wisteria.jpg'];
 
 
 function buildSlideShow(arr) {
